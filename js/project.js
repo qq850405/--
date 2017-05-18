9f2b3aa1ace179ba54f6393b48b6acd5f0a73987
Andy7596183 <ert7596183@gmail.com>
2017-05-19 00:51:53 +0800

    0519
    0519

--- js/project.js
+++ js/project.js
@@ -1,5 +1,8 @@
 $(function() {
-    
+
+    //$('h1').fadeIn(750).next().fadeIn(1800);
+    $('h1').animate({'opacity': '1'}, 750).delay(250).next().animate({'opacity': '1'}, 750).next().delay(500).animate({'opacity': '1'}, 750).next().delay(750).animate({'opacity': '1'}, 750).next().delay(1000).animate({'opacity': '1'}, 750);
+    $('.progress > .bar').addClass('start');
 
     $(document).on('click', function (e) {
         if($(e.target).closest(".mobile-nav,.close").length === 0) {
@@ -7,17 +10,61 @@ $(function() {
             $('.content, .project-pic > img').removeClass('is-overlayed').addClass('no-overlayed');
         }
     });
+
+    var i = 0;
+
+    if(i == 1350) {
+        clearInterval(number);
+    }
+
+    var number = setInterval(function (){
+        if(i <= 90){
+            $('.deadline').html(i);
+        }
+        if(i <= 150){
+            $('.num').html(i);
+        }
+        i++;
+    }, 10);
+
+    var k = 0;
+
+    if(k == 1350) {
+        clearInterval(number1);
+    }
+
+    var number1 = setInterval(function (){
+        
+        if(k <= 1350){
+            $('.total').html('$'+k);
+        }
+        k = k + 10;
+    }, 10);
+    
+    
+    
+    
+    
     
 
     $(window).scroll(function() {
         var scrollTop = $(this).scrollTop();
-
-        if(scrollTop > 0){
-            $('.navbar, .nav-brand, .nav-menu, .nav-menu > li, .dropdown-list').addClass('is-shrinked');
-        }else{
-            $('.navbar, .nav-brand, .nav-menu, .nav-menu > li, .dropdown-list').removeClass('is-shrinked')
-        }
+            //navbar animation
+            if(scrollTop > 0){
+                $('.navbar').addClass('white is-shrinked');
+                $('.underline').addClass('black-underline');
+                $('.nav-menu, .nav-brand, .nav-menu > li, li.nav-search, .dropdown-list, .nav-search > .icon-search-open').addClass('is-shrinked');
+                
+            }else if(scrollTop == 0){
+                $('.navbar').removeClass('white is-shrinked');
+                $('.underline').removeClass('black-underline');
+                $('.nav-menu, .nav-brand, .nav-menu > li, li.nav-search, .dropdown-list, .nav-search > .icon-search-open').removeClass('is-shrinked');
+                
+            }
     });
+
+    //paralax
+
 
     //textarea
     CKEDITOR.replace('textarea');
